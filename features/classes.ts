23dd5701320e435dc1f6e10eaf9fc274b8db865e
fabogit <fabo.dev.email@gmail.com>
2023-06-03 21:26:47 +0200

    #20
    Classes fields, contructor, inheritance and super

--- features/classes.ts
+++ features/classes.ts
@@ -1,6 +1,12 @@
 class Vehicle {
+  constructor(public color: string) {
+    // this.color = color; not needed if using public/protected/private modifiers
+  }
   protected start(): void {
     console.log('Starting!');
+  }
+  protected honk(): void {
+    console.log('Beep!');
   }
   stop(): void {
     console.log('Stopping!');
@@ -9,6 +15,9 @@ class Vehicle {
 
 // Child class override super/parent method
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
   private drive(): void {
     console.log('Car is starting!');
   }
@@ -18,6 +27,7 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+const car = new Car(4, 'red');
+console.log(car.color);
 car.startDriving();
 car.stop();
