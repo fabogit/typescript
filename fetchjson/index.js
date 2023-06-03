38172a37c001bbe3d079e8fe02a7a204640cb51d
fabogit <fabo.dev.email@gmail.com>
2023-06-03 14:51:31 +0200

    #3
    fetch api, add interface

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1.default.get(url).then(function (response) {
-    console.log(response.data);
+    // const todo: ITodo = response.data;
+    var todo = response.data;
+    var id = todo.id, title = todo.title, completed = todo.completed;
+    console.log("\n\t\tThe Todo with id: ".concat(id, "\n\t\tHas a title of: ").concat(title, "\n\t\tIs it finished: ").concat(completed, "\n\t"));
 });
