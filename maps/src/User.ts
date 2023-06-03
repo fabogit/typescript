d346eae3561d17a6f0dd052042888a629694edec
fabogit <fabo.dev.email@gmail.com>
2023-06-03 23:28:57 +0200

    #22
    User Class and generate random data

---
+++ maps/src/User.ts
@@ -0,0 +1,16 @@
+import { faker } from '@faker-js/faker';
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lon: number;
+  };
+  constructor() {
+    this.name = faker.person.firstName();
+    this.location = {
+      lat: faker.location.latitude(),
+      lon: faker.location.longitude(),
+    };
+  }
+}
