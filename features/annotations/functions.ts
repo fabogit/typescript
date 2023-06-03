680bf852ea12e4aa9ac49a521571c683b1085a49
fabogit <fabo.dev.email@gmail.com>
2023-06-03 17:12:10 +0200

    #12
    function destructuring arguments with annotations

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -32,3 +32,22 @@ const maybeError = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+// Destructuring with Annotations
+const todaysWheater = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(todaysWheater);
+
+// ES2015 - Destructuring then Annotate
+const ES15 = ({ date, weather }: { date: Date; weather: string }): void => {
+  console.log(date);
+  console.log(weather);
+};
