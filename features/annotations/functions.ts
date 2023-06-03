7b177a83f41a3a73309b1e3825cdf5d9a466f20a
fabogit <fabo.dev.email@gmail.com>
2023-06-03 17:04:16 +0200

    #11
    void/never

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -15,3 +15,20 @@ function divide(a: number, b: number): number {
 const multilpy = function (a: number, b: number): number {
   return a * b;
 };
+
+// Return void
+const logger = (message: string): void => {
+  console.log(message);
+  // return undefined
+};
+
+const maybeError = (message: string): void => {
+  if (!message) {
+    throw new Error('Err!');
+  }
+};
+
+// Return never, when unreachable code and function will never return anything
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
