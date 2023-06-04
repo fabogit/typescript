5b39fe3d24eda79673c1ba6b37998ec54c3d8e04
fabogit <fabo.dev.email@gmail.com>
2023-06-04 14:29:28 +0200

    #25
    render map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,4 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
 
@@ -6,3 +7,9 @@ const company = new Company();
 
 console.log(user);
 console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  center: { lat: 0, lng: 0 },
+  zoom: 2.25,
+  zoomControl: true,
+});
